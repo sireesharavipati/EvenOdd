2ff259841dd03a4580e6d0871278000ab9ea29fa
sireesharavipati <168215012+sireesharavipati@users.noreply.github.com>
2024-04-30 19:09:12 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+public class EvenOdd{
+public static void main(string[]args){
+int a[]={1,3,5,2,4,6,11,12,15,16};
+System.out.println("Even no.s in array.....");
+   for(int i=0;i<a.length;i++)
+      {
+         if(a[i]%2==0)
+        System.out.println("a[i]");
+        }
+System.out.println("Odd no.s in array.....");
+   for(int i=0;i<a.length;i++)
+      {
+         if(a[i]%2!=0)
+        System.out.println("a[i]");
+        }
+
+
+
+}
+}
